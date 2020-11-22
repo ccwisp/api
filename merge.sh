de4a3fef41e74d5e0657f58cfc40a465a8c8b202
ccwisp <armnecro@gmail.com>
2020-11-22 16:25:21 +0400

    sdas

--- merge.sh
+++ merge.sh
@@ -7,6 +7,7 @@ git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 printf '\nb2>>>>>>>>>>>>>>'
 
+git stash
 git checkout main || exit
 git merge --no-ff 'origin/dev' || exit
 
