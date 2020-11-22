5b8dc8ddb4186efe3a4c367f335de20ba92889f3
ccwisp <armnecro@gmail.com>
2020-11-22 14:53:12 +0400

    up

---
+++ merge.sh
@@ -0,0 +1,15 @@
+export GIT_COMMITTER_EMAIL='Travis@travisservice.com'
+export GIT_COMMITTER_NAME='ccwisp'
+
+printf '1>>>>>>>>>>>>>.\n'
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+git fetch --all || exit
+
+printf '\nb2>>>>>>>>>>>>>>'
+git checkout master || exit
+git merge --no-ff origin/dev || exit
+
+printf '3>>>>>>>>>\n'
+git push @github.com/"https://${GITHUB_TOKEN}@github.com/ShroukMansour/E-learning.git"
+
+printf '4>>>>>>>>>>>>n'
