c49b81fc6a98b66cc86ac36c6a8332c1a47c1b58
ccwisp <armnecro@gmail.com>
2020-11-22 17:05:09 +0400

    test

--- merge.sh
+++ merge.sh
@@ -5,12 +5,12 @@ git fetch --all || exit
 printf '\nb2>>>>>>>>>>>>>>'
 
 git stash
-git checkout 'origin/main' || exit
+git checkout main || exit
 git merge --no-ff 'origin/dev' || exit
 git stash pop
 
 printf '3>>>>>>>>>\n'
-git push "https://${GITHUB_TOKEN}@github.com/ccwisp/api.git"
+git push "https://ccwisp:${GITHUB_TOKEN}@github.com/ccwisp/api.git"
 
 
 
