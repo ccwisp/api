80f73689ac433dc6a4187307e945748aa5af323a
ccwisp <armnecro@gmail.com>
2020-11-22 16:27:41 +0400

    commit

---
+++ merge.sh
@@ -0,0 +1,19 @@
+
+export GIT_COMMITTER_EMAIL='Travis@travisservice.com'
+export GIT_COMMITTER_NAME='AutoMerge Bot'
+
+printf '1>>>>>>>>>>>>>.\n'
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+git fetch --all || exit
+printf '\nb2>>>>>>>>>>>>>>'
+
+git stash
+git checkout main || exit
+git merge --no-ff 'origin/dev' || exit
+
+printf '3>>>>>>>>>\n'
+git push "https://${GITHUB_TOKEN}@github.com/ccwisp/api.git"
+
+
+
+printf '4>>>>>>>>>>>>n'
